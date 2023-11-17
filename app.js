4b4a1f1eb95f0c5a76e8c3ccc43f57c982a41512
Ahmed-Ikram <139263388+Ahmed-Ikram@users.noreply.github.com>
2023-11-17 08:59:18 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const changeType = () => {
+    const password = document.getElementById("password");
+    const box = document.getElementById(`box`);
+    box.checked ? (password.type = `text`) : (password.type = `password`);
+  };
